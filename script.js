297e98ace2d066ff38fc0fd3771bdb1e54f2763a
monzartplay <monzartplay@gmail.com>
2026-07-25 07:52:22 +0300

    Add welcome message and button click alert

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+console.log("Welcome to MONZART!");
 
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+alert("Welcome to Monzart Studios!");
+});
+});
